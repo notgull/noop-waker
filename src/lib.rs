9adf4438e3643e7b43550e4cb1baffbb3d0d684b
not_a_seagull <notaseagull048@gmail.com>
2021-07-26 15:31:49 -0700

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -2,24 +2,24 @@
 
 //! A waker that does nothing when it is woken. Useful for "now or never" type scenarioes where a future is
 //! unlikely to be polled more than once, or for "spinning" executors.
-//! 
+//!
 //! # Example
-//! 
+//!
 //! A very inefficient implementation of the `block_on` function that polls the future over and over.
-//! 
+//!
 //! ```
 //! use core::{future::Future, hint::spin_loop, task::{Context, Poll}};
 //! use futures_lite::future::poll_fn;
 //! use noop_waker::noop_waker;
-//! 
+//!
 //! fn block_on<R>(f: impl Future<Output = R>) -> R {
 //!     // pin the future to the stack
 //!     futures_lite::pin!(f);
-//! 
+//!
 //!     // create the context
 //!     let waker = noop_waker();
 //!     let mut ctx = Context::from_waker(&waker);
-//! 
+//!
 //!     // poll future in a loop
 //!     loop {
 //!         match f.as_mut().poll(&mut ctx) {
@@ -28,9 +28,9 @@
 //!         }
 //!     }
 //! }
-//! 
+//!
 //! // this future returns pending 5 times before returning ready
-//! 
+//!
 //! let mut counter = 0;
 //! let my_future = poll_fn(|ctx| {
 //!     if counter < 5 {
@@ -41,21 +41,24 @@
 //!         Poll::Ready(7)
 //!     }
 //! });
-//! 
+//!
 //! assert_eq!(block_on(my_future), 7);
 //! ```
 
 #![no_std]
 #![warn(clippy::pedantic)]
 
-use core::{ptr, task::{Waker, RawWaker, RawWakerVTable}};
+use core::{
+    ptr,
+    task::{RawWaker, RawWakerVTable, Waker},
+};
 
 /// The whole point. Returns a waker that does nothing.
 #[inline]
 #[must_use]
 pub fn noop_waker() -> Waker {
     let raw = RawWaker::new(ptr::null(), &NOOP_WAKER_VTABLE);
-    
+
     // SAFETY: the contracts for RawWaker and RawWakerVTable are upheld
     unsafe { Waker::from_raw(raw) }
 }
